950d434a3bb59b493705088c3cb6135ffd99dfb5
Vel-murugan01396 <126084681+Vel-murugan01396@users.noreply.github.com>
2026-01-27 09:27:49 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import './globals.css'
 export const metadata = {
   title: 'Portfolio | Frontend Developer',
   description: 'Portfolio - Frontend, React & Next.js Developer',
+   icons: {
+    icon: '/developer.png',          // browser tab icon
+    apple: '/developer.png' // optional (for iOS)
+  },
 }
 
 export default function RootLayout({ children }) {
